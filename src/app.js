0f4c11dbeffd25fc9a10f947aa1c56a877aca04d
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-04 17:49:29 +0200

    Add accordion component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from "react";
 import JumbotronContainer from "./containers/jumbotron";
 import FooterContainer from "./containers/footer";
+import FaqsContainer from "./containers/faqs";
 
-// Finished at 55:00 (GlobalStyles)
+// Finished at 02:00 (Accordion Component Review)
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
