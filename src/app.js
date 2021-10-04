91951aaabd0dddb319e30853d6f77df069839813
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-04 16:25:27 +0200

    Add footer component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import React from "react";
 import JumbotronContainer from "./containers/jumbotron";
+import FooterContainer from "./containers/footer";
 
 // Finished at 55:00 (GlobalStyles)
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
