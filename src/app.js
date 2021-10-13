3e566ae7134570312635877c77c3b62d169ce340
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-13 12:58:44 +0200

    Create helper route that checks if user is signed in

--- src/app.js
+++ src/app.js
@@ -2,21 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect } from "./helpers/routes";
 
 // Finished at 04:01 (SignUp Page)
 
 export default function App() {
+  const user = false;
+
   return (
     // App routing
     <Router>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        exact
+        path={ROUTES.SIGN_IN}
+      >
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        exact
+        path={ROUTES.SIGN_UP}
+      >
+        <Signup />
+      </IsUserRedirect>
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup />
       </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
---
+++ src/helpers/routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+// Custom Route that redirect users accordingly to whether they are logged in or not
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    // Spread the rest of props on Route (exact, path)
+    // If user is false the children will be the Sign in component page
+    // If user is true, redirect them to loggedInPath
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return <Redirect to={{ pathname: loggedInPath }} />;
+        }
+
+        return null;
+      }}
+    />
+  );
+}
