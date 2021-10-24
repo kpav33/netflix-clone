d5a806740f68b30588c41d7905f98f2974750b8a
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-24 15:19:37 +0200

    Header dropdown styling

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -40,6 +40,11 @@ export default function BrowseContainer({ slides }) {
                   <Header.Picture src={user.photoURL} />
                   <Header.TextLink>{user.displayName}</Header.TextLink>
                 </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign Out
+                  </Header.TextLink>
+                </Header.Group>
               </Header.Dropdown>
             </Header.Profile>
           </Header.Group>
